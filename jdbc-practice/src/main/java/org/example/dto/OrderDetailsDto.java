6232c9f3805e1983f4735c3f1de98972b6e395c5
PiyumalNipuna60 <nipuna315np@gmail.com>
2024-05-12 01:39:57 +0530

    transaction content complete2

---
+++ jdbc-practice/src/main/java/org/example/dto/OrderDetailsDto.java
@@ -0,0 +1,18 @@
+package org.example.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class OrderDetailsDto {
+    private String itemCode;
+    private String description;
+    private double price;
+    private int qty;
+    private double totalPrice;
+}
